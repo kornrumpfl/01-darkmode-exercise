fd5fc577bef8a02fcd912d004196d1b63d06f4f0
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-29 17:01:18 +0200

    Solution

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import './styles.css'
+import Toggle from './components/Toggle'
+
+export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true)
+
+useEffect(()=>{
+alert(isDarkMode);
+},[isDarkMode])
+
+  return (
+    <main className={isDarkMode ? 'dark' : 'light'}>
+      <h1>You should use Dark Mode</h1>
+      <h2>Toggle Dark Mode</h2>
+      <Toggle toggleValue={isDarkMode} onChange={setIsDarkMode} />
+    </main>
+  )
+}
